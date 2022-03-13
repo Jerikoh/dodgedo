84d8511d23fd0229669b86eb7f05038748ce0d9e
Jere <jerebizai@gmail.com>
2022-03-13 20:24:22 -0300

    Previo a Loader

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,43 @@ const app = new Application({
 	width: 1024,
 	height: 720
 });
+
+console.log("Implementando background...");
+
+/*fondo
+entiendo que debería definir mejor el anchor para ubicarlo mas facil
+ni se si hay un método para definir fondos [] */
+const background: Sprite = Sprite.from("stage2.png");
+background.anchor.set(0.5);
+background.x = 300;
+background.y = 240;
+app.stage.addChild(background);
+
+console.log("Implementando character...");
+
 /*crea objeto tipo sprite (no se por que const y no let)
 en vez de crear con "new Sprite({" pixi tiene metodo estatico "Sprite.from" */
-const clampy: Sprite = Sprite.from("clampy.png");
+const character: Sprite = Sprite.from("ryu-hq.gif");
 /*define ancla o punto de referencia en el sprite para manipular posicion
-lo que no entiendo es que valor es ese 0.5, un eje? */
-clampy.anchor.set(0.5);
+lo que no entiendo es que valor es ese 0.5, un eje?
+por defecto es arriba a la izquierda */
+character.anchor.set(0.5);
 /*define posición del sprite en pantalla, al caso al centro via dividir por 2 sus medidas X Y
 las coordenadas 0,0 serían arriba a la izquierda */
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-/*agrega el sprite a la pantalla, no se que sería "stage"[]
+character.x = app.screen.width / 2;
+character.y = app.screen.height - 250;
+/*agrega el sprite a la pantalla, no se que sería "stage" []
 y si el "addChild" tiene que ver con alguna estructura clasica */
-app.stage.addChild(clampy);
+app.stage.addChild(character);
+
+console.log("Todos los sprites implementados.");
+console.log("Background",background.width,"x",background.height);
+console.log("Character",character.width,"x",character.height);
+/*tira que sus dimensiones son 1x1, porque todavía no se cargó la imagen
+para hacer que consulte un sprite cargado, usamos un loader (clase de pixi)
+usando su nombre en lugar de una URL al crear un sprite lograremos el cometido
+no se que es una "referencia" de Loader cuando habla de .shared
+se puede crear un loader no compartido (no se en que casos convendría)
+algunas ventajas de usar el loader:
+- usamos el nombre del elemento en vez de su dirección URL
+- evitamos tener que escribir toda una url (lol idem) */
